c2bf8568e067a376d470ee1602b9fad021eae3e9
hankaj <hanna.jarlaczynska@gmail.com>
2021-12-18 13:23:10 +0100

    Iteration 1

---
+++ application/iteration1/src/main/java/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String tel;
+    private String gender;
+    private int birthdate;
+    private String language;
+    private String country;
+    private String city;
+    private String address;
+    private String password;
+    private String role;
+    private boolean isEmailVerified;
+
+}
