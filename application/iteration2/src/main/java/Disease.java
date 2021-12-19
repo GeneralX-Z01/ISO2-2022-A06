14b23652b4a42a7c30926a8699850db40329dd53
juliakahan <kahanjulia@gmail.com>
2021-12-19 14:12:43 +0100

    Update Disease

---
+++ application/iteration2/src/main/java/Disease.java
@@ -0,0 +1,35 @@
+public class Disease {
+
+    private int frstCase;
+    private String variety;
+    private String Symptoms;
+    private String name;
+    private String ID;
+    private String referredAgeGroup;
+
+    public void addDisease() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public void getAllDisease() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public void deleteDisease() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public void updateDisease() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    public void getDisease() {
+
+        throw new UnsupportedOperationException();
+    }
+
+}
