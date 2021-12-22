f15b519abe8abf71fb4110efdbc217c45413c5e9
bafode <bafode.camara@ogr.sakarya.edu.tr>
2021-12-22 13:53:36 +0100

    UC5 implementation

---
+++ application/iteration5/src/Vaccination.java
@@ -0,0 +1,36 @@
+package pro;
+
+public class Vaccination {
+
+	private String id;
+	private String type;
+	private String date;
+	private String description;
+	private String doseNumber;
+	private String receiver;
+
+	public void create() {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void getVaccination() {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void getSingleVaccination() {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateVaccination() {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteVaccination() {
+
+		throw new UnsupportedOperationException();
+	}
+}
